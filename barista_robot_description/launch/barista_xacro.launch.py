7fd23034d2826e99c0449a2330b56d560e31fb3e
Pan <pangineering@gmail.com>
2023-07-19 17:31:38 +0000

    part2 update

---
+++ barista_robot_description/launch/barista_xacro.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_prefix
+
+import xacro
+
+
+def generate_launch_description():
+
+ 
+    package_description = "barista_robot_description"
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={"verbose": "false", 'pause': 'true'}.items(),
+    )
+
+    robot_model_path = os.path.join(
+        get_package_share_directory('barista_robot_description'))
+
+    xacro_file = os.path.join(robot_model_path, 'xacro', 'barista_robot_model.urdf.xacro')
+
+    # convert XACRO file into URDF
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'barista', '-x', '1.0', '-y', '1.0', '-z', '0.2',
+                                   '-topic', 'robot_description'],
+                        output='screen')
+
+
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'robot.rviz')
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir]
+    )
+
+
+    return LaunchDescription([
+        gazebo,
+        spawn_entity,
+        robot_state_publisher,
+        joint_state_publisher_node,
+        rviz_node
+    ])
